a612e19d5045eebb7427cb30a0b2e19e85619ec1
Young879 <wibicpt401@gmail.com>
2024-11-10 21:37:44 +0700

    Wahyoeeeee

---
+++ Tugas 2 Pertemuan 6.py
@@ -0,0 +1,14 @@
+#Nama:Dwi Cahyo Wibisono
+#NIM : 2402053
+#Kelas: RPL 1A
+
+x = int(input("Masukkan Nilai: "))
+
+if (x % 2):
+    print("adalah bilangan ganjil")
+    if ( x < 0):
+        print("dan bilangan negatif")
+    else:
+        print("dan bilangan positif")
+else:
+    print("adalah bilangan positif genap")
